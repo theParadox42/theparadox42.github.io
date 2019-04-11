d85c009a4d456d92fc861434c448524350e86ec5
Paradox Programming <41707197+theParadox42@users.noreply.github.com>
2019-04-11 14:38:18 -0600

    Fixed error code

---
+++ js/master.js
@@ -0,0 +1,25 @@
+function loadScript(url, callback) {
+    // Adding the script tag to the head as suggested before
+    var body = document.querySelector("body");
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    body.appendChild(script);
+}
+function doneLoading(){
+    alert("JS has been added in!")
+};
+function loadBootstrap(){
+    loadScript("/assets/libraries/bootstrap/js/bootstrap.min.js",doneLoading);
+};
+function loadPopper() {
+    loadScript("/assets/libraries/popper.min.js", loadBootstrap)
+};
+loadScript("/assets/libraries/jquery-3.4.0.min.js", loadPopper);
