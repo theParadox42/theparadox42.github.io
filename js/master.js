ef0bd7773118b70db90d7dd3d08c7cf13a15b0ff
Paradox Programming <41707197+theParadox42@users.noreply.github.com>
2019-04-12 11:45:52 -0600

    Update master.js

--- js/master.js
+++ js/master.js
@@ -6,11 +6,13 @@ function loadScript(url, callback) {
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
-
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
-    script.onreadystatechange = callback;
-    script.onload = callback;
+    
+    if(callback){
+        // Then bind the event to the callback function.
+        // There are several events for cross browser compatibility.
+        script.onreadystatechange = callback;
+        script.onload = callback;
+    }
 
     // Fire the loading
     body.appendChild(script);
