5f0d6f5da387618a7b91b45b6036aaeab5657bec
Paradox Programming <nathaniel.fargo@gmail.com>
2019-05-06 18:51:36 -0600

    yeah

--- js/networkdisplay.js
+++ js/networkdisplay.js
@@ -1,6 +1,18 @@
 
 var networkContainer = document.getElementsByClassName("network-container")[0];
-console.log(networkContainer.style);
+console.log(networkContainer);
+
+function setup(){
+    var canvas = createCanvas()
+    canvas.parent("network-container");
+    canvas.elt.style.width = "100%";
+    canvas.elt.style.height = "300px";
+    background(0, 0, 0)
+}
+
+function draw(){
+    background(0, 0, 0)
+}
 
 function windowResized(){
     resizeCanvas();
